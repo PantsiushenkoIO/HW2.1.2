ddab088775925b34b84f4d2916ceee3e9a7a6051
PantsiushenkoIO <baksmog@gmail.com>
2021-05-19 00:12:46 +0300

    Initial Commit

---
+++ HW2.1.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.1.2
+//
+//  Created by Igor on 19.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
